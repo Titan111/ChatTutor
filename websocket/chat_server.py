b6f942e5e5da1b11bcd977509bf76ea84e1cdbc7
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2018-02-16 15:51:05 +0900

    プログラムにコメントを追加

--- websocket/chat_server.py
+++ websocket/chat_server.py
@@ -1,21 +1,25 @@
 from websocket_server import WebsocketServer
  
 # Callback functions
- 
 def new_client(client, server):
-	pass
+	pass#何もしない
 
 def client_left(client, server):
-	pass
+	pass#何もしない
  
 def message_received(client, server, message):
+	#メッセージ内容を表示
 	print(message)
+	#すべてのクライエントにmessageを送信
 	server.send_message_to_all(message)
  
 # Main
 if __name__ == "__main__":
-	server = WebsocketServer(port=12345, host='172.28.32.133', )
+	#オブジェクトを生成
+	server = WebsocketServer(port=12345, host='127.0.0.1', )
+	#関数を登録
 	server.set_fn_new_client(new_client)
 	server.set_fn_client_left(client_left)
 	server.set_fn_message_received(message_received)
+	#サーバーを開始
 	server.run_forever()
